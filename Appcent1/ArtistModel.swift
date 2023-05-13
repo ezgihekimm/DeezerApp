56914351307f040ac84e9c4ecb05498a30842ba4
ezgi.hekim.2000 <ezgi.hekim.2000@gmail.com>
2023-05-13 19:49:45 +0300

    header added

---
+++ Appcent1/ArtistModel.swift
@@ -0,0 +1,21 @@
+//
+//  ArtistModel.swift
+//  Appcent1
+//
+//  Created by Ezgi Hekim on 13.05.2023.
+//
+
+import Foundation
+
+struct Artist: Codable{
+    
+    let id: Int
+    let name: String
+    let picture: String
+    
+    init(id: Int, name: String, picture: String) {
+        self.id = id
+        self.name = name
+        self.picture = picture
+    }
+}
---
+++ Appcent1/CategoryModel.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryModel.swift
+//  Appcent1
+//
+//  Created by Ezgi Hekim on 13.05.2023.
+//
+
+import Foundation
+
+struct Category: Codable{
+    
+    let id: Int
+    let name: String
+    let picture: String
+    
+    init(id: Int, name: String, picture: String) {
+        self.id = id
+        self.name = name
+        self.picture = picture
+    }
+}
